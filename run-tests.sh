896e039fc98c6e6bf26816fd3d00dc536ca0db0d
David Caro <david@dcaro.es>
2016-09-30 20:04:43 +0200

    tests: add module files to pep8 check from pytest
    Signed-off-by: David Caro <david@dcaro.es>

--- run-tests.sh
+++ run-tests.sh
@@ -26,6 +26,12 @@
 
 pydocstyle --explain --source inspire_schemas tests && \
 isort -rc -c --skip docs/conf.py -df **/*.py && \
-pytest --pep8 --cov=inspire_schemas --cov-report=term-missing -vv tests && \
+pytest \
+    --pep8 \
+    --cov=inspire_schemas \
+    --cov-report=term-missing \
+    -vv \
+    tests \
+    inspire_schemas && \
 sphinx-build -qnN docs docs/_build/html && \
 sphinx-build -qnN -b doctest docs docs/_build/doctest
