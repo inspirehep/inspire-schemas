7308a1946c3bdba2a75e88da9046a54d0f885a78
David Caro <david@dcaro.es>
2016-10-09 20:15:12 +0200

    packaging: move to autosemver
    * NEW Using the external `autosemver` module instead of in-repo script
      to handle versioning, authors, changelog and release notes
      generation.
    
    Signed-off-by: David Caro <david@dcaro.es>

--- setup.py
+++ setup.py
@@ -24,135 +24,33 @@
 
 """INSPIRE schemas and related tools bundle."""
 
-import os
-import subprocess
-
 from setuptools import setup, find_packages
 
+from autosemver.packaging import (
+    get_authors,
+    get_current_version,
+    get_changelog,
+)
 
-def check_output(args):
-    proc = subprocess.Popen(
-        args,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-    )
-    stdout, stderr = proc.communicate()
-
-    if proc.returncode:
-        raise RuntimeError(
-            'Failed to run %s\nrc=%s\nstdout=\n%sstderr=%s' %
-            (args, proc.returncode, stdout, stderr)
-        )
-
-    return stdout.decode()
-
-
-def get_version(project_dir=os.curdir):
-    """
-    Retrieves the version of the package, from the PKG-INFO file or generates
-    it with the version script
-    Returns:
-        str: Version for the package
-    Raises:
-        RuntimeError: If the version could not be retrieved
-    """
-    if (
-        'INSPIRE_SCHEMAS_VERSION' in os.environ and
-        os.environ['INSPIRE_SCHEMAS_VERSION']
-    ):
-        return os.environ['INSPIRE_SCHEMAS_VERSION']
-
-    version = None
-    pkg_info_file = os.path.join(project_dir, 'PKG-INFO')
-    version_manager = os.path.join(project_dir, 'scripts/version_manager.py')
-    if os.path.exists(pkg_info_file):
-        with open(pkg_info_file) as info_fd:
-            for line in info_fd.readlines():
-                if line.startswith('Version: '):
-                    version = line.split(' ', 1)[-1]
-
-    elif os.path.exists(version_manager):
-        version = check_output(
-            [version_manager, project_dir, 'version']
-        ).strip()
-
-    if version is None:
-        raise RuntimeError('Failed to get package version')
-
-    # py3 compatibility step
-    if not isinstance(version, str) and isinstance(version, bytes):
-        version = version.decode()
-
-    return version
-
-
-def get_authors(project_dir=os.curdir):
-    """
-    Retrieves the authors list, from the AUTHORS file (if in a package) or
-    generates it with the version script
-    Returns:
-        list(str): List of authors
-    Raises:
-        RuntimeError: If the authors could not be retrieved
-    """
-    authors = set()
-    pkg_info_file = os.path.join(project_dir, 'PKG-INFO')
-    authors_file = os.path.join(project_dir, 'AUTHORS')
-    version_manager = os.path.join(project_dir, 'scripts/version_manager.py')
-    if os.path.exists(pkg_info_file) and os.path.exists(authors_file):
-        with open(authors_file) as authors_fd:
-            authors = set(authors_fd.read().splitlines())
-
-    elif os.path.exists(version_manager):
-        authors = set(check_output(
-            [version_manager, project_dir, 'authors']
-        ).strip().splitlines())
-
-    return authors
-
-
-def get_changelog(project_dir=os.curdir):
-    """
-    Retrieves the changelog, from the CHANGELOG file (if in a package) or
-    generates it with the version script
-    Returns:
-        str: changelog
-    Raises:
-        RuntimeError: If the changelog could not be retrieved
-    """
-    changelog = ''
-    pkg_info_file = os.path.join(project_dir, 'PKG-INFO')
-    changelog_file = os.path.join(project_dir, 'CHANGELOG')
-    version_manager = os.path.join(project_dir, 'scripts/version_manager.py')
-    if os.path.exists(pkg_info_file) and os.path.exists(changelog_file):
-        with open(changelog_file) as changelog_fd:
-            changelog = changelog_fd.read()
-
-    elif os.path.exists(version_manager):
-        changelog = check_output(
-            [version_manager, project_dir, 'changelog']
-        ).strip()
-
-    return changelog
-
+URL = 'https://github.com/inspirehep/inspire-schemas'
 
 if __name__ == '__main__':
     with open('AUTHORS', 'w') as authors_fd:
         authors_fd.write('\n'.join(get_authors()))
 
     with open('CHANGELOG', 'w') as changelog_fd:
-        changelog_fd.write(get_changelog())
+        changelog_fd.write(get_changelog(bugtracker_url=URL + '/issues/'))
 
     setup(
         author='CERN',
         author_email='admin@inspirehep.net',
         description='Inspire JSON schemas and utilities to use them.',
-        install_requires=['jsonschema'],
+        install_requires=['autosemver', 'jsonschema'],
         license='GPLv2',
         name='inspire-schemas',
         package_data={'': ['*.json', 'CHANGELOG', 'AUTHORS']},
         packages=find_packages(),
-        url='https://github.com/inspirehep/inspire-schemas',
-        version=get_version(),
+        url=URL,
+        version=get_current_version(project_name='inspire-schemas'),
         zip_safe=False,
     )
