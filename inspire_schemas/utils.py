ecd15f133dbbb8daad20e3f84ae8e26c35ed4ff2
Iuliana Voinea <iuliana.voinea@student.manchester.ac.uk>
2017-10-06 15:42:19 +0200

    utils: add arXiv utils
    Signed-off-by: Iuliana Voinea <iuliana.voinea@student.manchester.ac.uk>

--- inspire_schemas/utils.py
+++ inspire_schemas/utils.py
@@ -43,6 +43,254 @@ _schema_root_path = os.path.abspath(resource_filename(__name__, 'records'))
 
 _RE_2_CHARS = re.compile(r'[a-z].*[a-z]', re.I)
 
+# list produced from https://arxiv.org/archive/
+_NEW_CATEGORIES = {
+    'acc-phys': 'physics.acc-ph',
+    'adap-org': 'nlin.AO',
+    'alg-geom': 'math.AG',
+    'ao-sci': 'physics.ao-ph',
+    'atom-ph': 'physics.atom-ph',
+    'bayes-an': 'physics.data-an',
+    'chao-dyn': 'nlin.CD',
+    'chem-ph': 'physics.chem-ph',
+    'cmp-lg': 'cs.CL',
+    'comp-gas': 'nlin.CG',
+    'dg-ga': 'math.DG',
+    'funct-an': 'math.FA',
+    'mtrl-th': 'cont-mat.mtrl-sci',
+    'patt-sol': 'nlin.PS',
+    'plasm-ph': 'physics.plasm-ph',
+    'q-alg': 'math.QA',
+    'solv-int': 'nlin.SI',
+    'supr-con': 'cond-mat.supr-con',
+}
+
+ARXIV_TO_INSPIRE_CATEGORY_MAPPING = {
+    'alg-geom': 'Math and Math Physics',
+    'astro-ph': 'Astrophysics',
+    'astro-ph.CO': 'Astrophysics',
+    'astro-ph.EP': 'Astrophysics',
+    'astro-ph.GA': 'Astrophysics',
+    'astro-ph.HE': 'Astrophysics',
+    'astro-ph.IM': 'Instrumentation',
+    'astro-ph.SR': 'Astrophysics',
+    'cond-mat': 'General Physics',
+    'cond-mat.dis-nn': 'General Physics',
+    'cond-mat.mes-hall': 'General Physics',
+    'cond-mat.mtrl-sci': 'General Physics',
+    'cond-mat.other': 'General Physics',
+    'cond-mat.quant-gas': 'General Physics',
+    'cond-mat.soft': 'General Physics',
+    'cond-mat.stat-mech': 'General Physics',
+    'cond-mat.str-el': 'General Physics',
+    'cond-mat.supr-con': 'General Physics',
+    'cs': 'Computing',
+    'cs.AI': 'Computing',
+    'cs.AR': 'Computing',
+    'cs.CC': 'Computing',
+    'cs.CE': 'Computing',
+    'cs.CG': 'Computing',
+    'cs.CL': 'Computing',
+    'cs.CR': 'Computing',
+    'cs.CV': 'Computing',
+    'cs.CY': 'Computing',
+    'cs.DB': 'Computing',
+    'cs.DC': 'Computing',
+    'cs.DL': 'Computing',
+    'cs.DM': 'Computing',
+    'cs.DS': 'Computing',
+    'cs.ET': 'Computing',
+    'cs.FL': 'Computing',
+    'cs.GL': 'Computing',
+    'cs.GR': 'Computing',
+    'cs.GT': 'Computing',
+    'cs.HC': 'Computing',
+    'cs.IR': 'Computing',
+    'cs.IT': 'Computing',
+    'cs.LG': 'Computing',
+    'cs.LO': 'Computing',
+    'cs.MA': 'Computing',
+    'cs.MM': 'Computing',
+    'cs.MS': 'Computing',
+    'cs.NA': 'Computing',
+    'cs.NE': 'Computing',
+    'cs.NI': 'Computing',
+    'cs.OH': 'Computing',
+    'cs.OS': 'Computing',
+    'cs.PF': 'Computing',
+    'cs.PL': 'Computing',
+    'cs.RO': 'Computing',
+    'cs.SC': 'Computing',
+    'cs.SD': 'Computing',
+    'cs.SE': 'Computing',
+    'cs.SI': 'Computing',
+    'cs.SY': 'Computing',
+    'dg-ga': 'Math and Math Physics',
+    'gr-qc': 'Gravitation and Cosmology',
+    'hep-ex': 'Experiment-HEP',
+    'hep-lat': 'Lattice',
+    'hep-ph': 'Phenomenology-HEP',
+    'hep-th': 'Theory-HEP',
+    'math': 'Math and Math Physics',
+    'math-ph': 'Math and Math Physics',
+    'math.AC': 'Math and Math Physics',
+    'math.AG': 'Math and Math Physics',
+    'math.AP': 'Math and Math Physics',
+    'math.AT': 'Math and Math Physics',
+    'math.CA': 'Math and Math Physics',
+    'math.CO': 'Math and Math Physics',
+    'math.CT': 'Math and Math Physics',
+    'math.CV': 'Math and Math Physics',
+    'math.DG': 'Math and Math Physics',
+    'math.DS': 'Math and Math Physics',
+    'math.FA': 'Math and Math Physics',
+    'math.GM': 'Math and Math Physics',
+    'math.GN': 'Math and Math Physics',
+    'math.GR': 'Math and Math Physics',
+    'math.GT': 'Math and Math Physics',
+    'math.HO': 'Math and Math Physics',
+    'math.IT': 'Math and Math Physics',
+    'math.KT': 'Math and Math Physics',
+    'math.LO': 'Math and Math Physics',
+    'math.MG': 'Math and Math Physics',
+    'math.MP': 'Math and Math Physics',
+    'math.NA': 'Math and Math Physics',
+    'math.NT': 'Math and Math Physics',
+    'math.OA': 'Math and Math Physics',
+    'math.OC': 'Math and Math Physics',
+    'math.PR': 'Math and Math Physics',
+    'math.QA': 'Math and Math Physics',
+    'math.RA': 'Math and Math Physics',
+    'math.RT': 'Math and Math Physics',
+    'math.SG': 'Math and Math Physics',
+    'math.SP': 'Math and Math Physics',
+    'math.ST': 'Math and Math Physics',
+    'nlin': 'General Physics',
+    'nlin.AO': 'General Physics',
+    'nlin.CD': 'General Physics',
+    'nlin.CG': 'General Physics',
+    'nlin.PS': 'Math and Math Physics',
+    'nlin.SI': 'Math and Math Physics',
+    'nucl-ex': 'Experiment-Nucl',
+    'nucl-th': 'Theory-Nucl',
+    'patt-sol': 'Math and Math Physics',
+    'physics': 'General Physics',
+    'physics.acc-ph': 'Accelerators',
+    'physics.ao-ph': 'General Physics',
+    'physics.atm-clus': 'General Physics',
+    'physics.atom-ph': 'General Physics',
+    'physics.bio-ph': 'Other',
+    'physics.chem-ph': 'Other',
+    'physics.class-ph': 'General Physics',
+    'physics.comp-ph': 'Computing',
+    'physics.data-an': 'Data Analysis and Statistics',
+    'physics.ed-ph': 'Other',
+    'physics.flu-dyn': 'General Physics',
+    'physics.gen-ph': 'General Physics',
+    'physics.geo-ph': 'General Physics',
+    'physics.hist-ph': 'Other',
+    'physics.ins-det': 'Instrumentation',
+    'physics.med-ph': 'Other',
+    'physics.optics': 'General Physics',
+    'physics.plasm-ph': 'General Physics',
+    'physics.pop-ph': 'Other',
+    'physics.soc-ph': 'Other',
+    'physics.space-ph': 'Astrophysics',
+    'q-alg': 'Math and Math Physics',
+    'q-bio': 'Other',
+    'q-bio.BM': 'Other',
+    'q-bio.CB': 'Other',
+    'q-bio.GN': 'Other',
+    'q-bio.MN': 'Other',
+    'q-bio.NC': 'Other',
+    'q-bio.OT': 'Other',
+    'q-bio.PE': 'Other',
+    'q-bio.QM': 'Other',
+    'q-bio.SC': 'Other',
+    'q-bio.TO': 'Other',
+    'q-fin': 'Other',
+    'q-fin.CP': 'Other',
+    'q-fin.EC': 'Other',
+    'q-fin.GN': 'Other',
+    'q-fin.MF': 'Other',
+    'q-fin.PM': 'Other',
+    'q-fin.PR': 'Other',
+    'q-fin.RM': 'Other',
+    'q-fin.ST': 'Other',
+    'q-fin.TR': 'Other',
+    'quant-ph': 'General Physics',
+    'solv-int': 'Math and Math Physics',
+    'stat': 'Other',
+    'stat.AP': 'Other',
+    'stat.CO': 'Other',
+    'stat.ME': 'Other',
+    'stat.ML': 'Other',
+    'stat.OT': 'Other',
+    'stat.TH': 'Other',
+}
+
+
+def normalize_arxiv_category(category):
+    """Normalize arXiv category to be schema compliant.
+
+    This properly capitalizes the category and replaces the dash by a dot if
+    needed. If the category is obsolete, it also gets converted it to its
+    current equivalent.
+
+    Example:
+        >>> from inspire_schemas.utils import normalize_arxiv_category
+        >>> normalize_arxiv_category('funct-an')
+        u'math.FA'
+
+    """
+    category = _NEW_CATEGORIES.get(category.lower(), category)
+    for valid_category in valid_arxiv_categories():
+        if (category.lower() == valid_category.lower() or
+                category.lower().replace('-', '.') == valid_category.lower()):
+            return valid_category
+    return category  # XXX: will fail validation and be logged
+
+
+def valid_arxiv_categories():
+    """List of all arXiv categories that ever existed.
+
+    Example:
+        >>> from inspire_schemas.utils import valid_arxiv_categories
+        >>> 'funct-an' in valid_arxiv_categories()
+        True
+
+    """
+    schema = load_schema('elements/arxiv_categories')
+    categories = schema['enum']
+    categories.extend(_NEW_CATEGORIES.keys())
+
+    return categories
+
+
+def classify_field(value):
+    """Translate an arXiv category to the corresponding INSPIRE category.
+
+    Args:
+        value: arXiv category to translate
+
+    Returns:
+        str: ``None`` if ``value`` is not a string containing a valid arXiv
+            category, otherwise the corresponding INSPIRE category.
+
+    """
+    if not value:
+        return None
+    elif not isinstance(value, six.string_types):
+        return None
+    else:
+        casted_value = value.upper()
+        for name, category in six.iteritems(ARXIV_TO_INSPIRE_CATEGORY_MAPPING):
+            if name.upper() == casted_value:
+                return category
+            elif category.upper() == casted_value:
+                return category
+        return None
+
 
 def split_page_artid(page_artid):
     """Split page_artid into page_start/end and artid."""
