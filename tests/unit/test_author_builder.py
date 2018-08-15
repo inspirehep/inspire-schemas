9230facd4a713c643b151b55dbfcd6be43fe61d7
Victor Balbuena <vbalbp@gmail.com>
2018-08-15 16:58:39 +0200

    builders: add functionalities to author builder
    Signed-off-by: Victor Balbuena <vbalbp@gmail.com>

--- tests/unit/test_author_builder.py
+++ tests/unit/test_author_builder.py
@@ -27,7 +27,9 @@ from inspire_schemas.builders.authors import AuthorBuilder
 
 
 def test_author_builder_default_constructor():
-    expected = {}
+    expected = {
+        '_collections': ['Authors'],
+    }
     result = AuthorBuilder()
 
     assert expected == result.obj
@@ -78,6 +80,500 @@ def test_author_builder_set_name_can_be_called_multiple_times():
 
     expected = {'value': 'Torre, Riccardo'}
     result = author.obj['name']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_set_display_name():
+    schema = load_schema('authors')
+    subschema = schema['properties']['name']
+
+    author = AuthorBuilder()
+    author.set_name('Torre, Riccardo')
+    author.set_display_name('Rick')
+
+    expected = {
+        'value': 'Torre, Riccardo',
+        'preferred_name': 'Rick'
+    }
+    result = author.obj['name']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_set_display_name_can_be_called_multiple_times():
+    schema = load_schema('authors')
+    subschema = schema['properties']['name']
+
+    author = AuthorBuilder()
+    author.set_name('Torre, Riccardo')
+    author.set_display_name('Ricardo')
+    author.set_display_name('Rick')
+
+    expected = {
+        'value': 'Torre, Riccardo',
+        'preferred_name': 'Rick'
+    }
+    result = author.obj['name']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_native_name():
+    schema = load_schema('authors')
+    subschema = schema['properties']['name']
+
+    author = AuthorBuilder()
+    author.set_name('Torre, Riccardo')
+    author.add_native_name('Rick')
+
+    expected = {
+        'value': 'Torre, Riccardo',
+        'native_names': [
+            'Rick'
+        ]
+    }
+    result = author.obj['name']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_multiple_native_names():
+    schema = load_schema('authors')
+    subschema = schema['properties']['name']
+
+    author = AuthorBuilder()
+    author.set_name('Torre, Riccardo')
+    author.add_native_name('Rick')
+    author.add_native_name('Torrecillas')
+    author.add_native_name('Ricardo')
+
+    expected = {
+        'value': 'Torre, Riccardo',
+        'native_names': [
+            'Rick',
+            'Torrecillas',
+            'Ricardo',
+        ]
+    }
+    result = author.obj['name']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_email_address():
+    schema = load_schema('authors')
+    subschema = schema['properties']['email_addresses']
+
+    author = AuthorBuilder()
+    author.add_email_address('example@test.com')
+
+    expected = [{
+        "value": 'example@test.com'
+    }]
+    result = author.obj['email_addresses']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_multiple_email_addresses():
+    schema = load_schema('authors')
+    subschema = schema['properties']['email_addresses']
+
+    author = AuthorBuilder()
+    author.add_email_address('example@test.com')
+    author.add_email_address('test@example.com')
+
+    expected = [{
+        "value": 'example@test.com'
+    },
+        {
+        "value": 'test@example.com'
+    }]
+    result = author.obj['email_addresses']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_set_status():
+    schema = load_schema('authors')
+    subschema = schema['properties']['status']
+
+    author = AuthorBuilder()
+    author.set_status('active')
+
+    expected = 'active'
+    result = author.obj['status']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_url_without_description():
+    schema = load_schema('authors')
+    subschema = schema['properties']['urls']
+
+    author = AuthorBuilder()
+    author.add_url('https://www.example.com')
+
+    expected = [{
+        "value": "https://www.example.com"
+    }]
+    result = author.obj['urls']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_url_with_description():
+    schema = load_schema('authors')
+    subschema = schema['properties']['urls']
+
+    author = AuthorBuilder()
+    author.add_url('https://www.example.com', 'this is an example')
+
+    expected = [{
+        "value": "https://www.example.com",
+        "description": "this is an example"
+    }]
+    result = author.obj['urls']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_blog():
+    schema = load_schema('authors')
+    subschema = schema['properties']['urls']
+
+    author = AuthorBuilder()
+    author.add_blog('https://www.blog.com')
+
+    expected = [{
+        "value": "https://www.blog.com",
+        "description": "blog"
+    }]
+    result = author.obj['urls']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_linkedin():
+    schema = load_schema('authors')
+    subschema = schema['properties']['ids']
+
+    author = AuthorBuilder()
+    author.add_linkedin('https://www.linkedin.com/in/example-12345/')
+
+    expected = [{
+        "value": "https://www.linkedin.com/in/example-12345/",
+        "schema": "LINKEDIN"
+    }]
+    result = author.obj['ids']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_twitter():
+    schema = load_schema('authors')
+    subschema = schema['properties']['ids']
+
+    author = AuthorBuilder()
+    author.add_twitter('https://twitter.com/Example')
+
+    expected = [{
+        "value": "https://twitter.com/Example",
+        "schema": "TWITTER"
+    }]
+    result = author.obj['ids']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_research_field():
+    schema = load_schema('authors')
+    subschema = schema['properties']['arxiv_categories']
+
+    author = AuthorBuilder()
+    author.add_research_field('math.CV')
+
+    expected = [
+        "math.CV"
+    ]
+    result = author.obj['arxiv_categories']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_research_field_accepts_multiple_categories():
+    schema = load_schema('authors')
+    subschema = schema['properties']['arxiv_categories']
+
+    author = AuthorBuilder()
+    author.add_research_field('math.CV')
+    author.add_research_field('astro-ph.HE')
+    author.add_research_field('econ.EM')
+
+    expected = [
+        "math.CV",
+        "astro-ph.HE",
+        "econ.EM",
+    ]
+    result = author.obj['arxiv_categories']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_institution():
+    schema = load_schema('authors')
+    subschema = schema['properties']['positions']
+
+    author = AuthorBuilder()
+    author.add_institution(institution='Colgate University',
+                           start_date='1994-02-01',
+                           end_date='1995-01-31',
+                           rank='PHD',
+                           record={
+                                "$ref": "http://180"
+                           },
+                           curated=True,
+                           current=False)
+
+    expected = [{
+        "institution": 'Colgate University',
+        "start_date": u'1994-02-01',
+        "end_date": u'1995-01-31',
+        "rank": 'PHD',
+        "record": {
+            "$ref": "http://180"
+        },
+        "curated_relation": True,
+        "current": False
+    }]
+    result = author.obj['positions']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_institution_normalizes_start_date():
+    schema = load_schema('authors')
+    subschema = schema['properties']['positions']
+
+    author = AuthorBuilder()
+    author.add_institution(institution='Colgate University',
+                           start_date='February 1 1994')
+
+    expected = [{
+        "institution": 'Colgate University',
+        "start_date": u'1994-02-01',
+        "curated_relation": False,
+        "current": False
+    }]
+    result = author.obj['positions']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_institution_normalizes_end_date():
+    schema = load_schema('authors')
+    subschema = schema['properties']['positions']
+
+    author = AuthorBuilder()
+    author.add_institution(institution='Colgate University',
+                           end_date='31 January 2005')
+
+    expected = [{
+        "institution": 'Colgate University',
+        "end_date": u'2005-01-31',
+        "curated_relation": False,
+        "current": False
+    }]
+    result = author.obj['positions']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_project():
+    schema = load_schema('authors')
+    subschema = schema['properties']['project_membership']
+
+    author = AuthorBuilder()
+    author.add_project(name='pariatur',
+                       start_date='1997-05-01',
+                       end_date='2001-12-31',
+                       record={
+                            "$ref": "http://180"
+                       },
+                       curated=True,
+                       current=True)
+
+    expected = [{
+        "name": 'pariatur',
+        "start_date": u'1997-05-01',
+        "end_date": u'2001-12-31',
+        "record": {
+            "$ref": "http://180"
+        },
+        "curated_relation": True,
+        "current": True
+    }]
+    result = author.obj['project_membership']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_project_normalizes_start_date():
+    schema = load_schema('authors')
+    subschema = schema['properties']['project_membership']
+
+    author = AuthorBuilder()
+    author.add_project(name='pariatur',
+                       start_date='1999 February')
+
+    expected = [{
+        "name": 'pariatur',
+        "start_date": u'1999-02',
+        "curated_relation": False,
+        "current": False
+    }]
+    result = author.obj['project_membership']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_institution_normalizes_end_date():
+    schema = load_schema('authors')
+    subschema = schema['properties']['project_membership']
+
+    author = AuthorBuilder()
+    author.add_project(name='pariatur',
+                       end_date='5 2016 January')
+
+    expected = [{
+        "name": 'pariatur',
+        "end_date": u'2016-01-05',
+        "curated_relation": False,
+        "current": False
+    }]
+    result = author.obj['project_membership']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_comment():
+    schema = load_schema('authors')
+    subschema = schema['properties']['_private_notes']
+
+    author = AuthorBuilder()
+    author.add_comment(comment='this is an example',
+                       source='curator')
+
+    expected = [{
+        "value": 'this is an example',
+        "source": 'curator'
+    }]
+    result = author.obj['_private_notes']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_comment_without_source():
+    schema = load_schema('authors')
+    subschema = schema['properties']['_private_notes']
+
+    author = AuthorBuilder()
+    author.add_comment('this is an example')
+
+    expected = [{
+        "value": 'this is an example'
+    }]
+    result = author.obj['_private_notes']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_advisor():
+    schema = load_schema('authors')
+    subschema = schema['properties']['advisors']
+
+    author = AuthorBuilder()
+    author.add_advisor(name='Torres, Riccardo',
+                       ids=[{
+                            "schema": "DESY",
+                            "value": "DESY-55924820881"
+                            },
+                            {
+                            "schema": "SCOPUS",
+                            "value": "7039712595"
+                            },
+                            {
+                            "schema": "SCOPUS",
+                            "value": "8752067273"
+                            }],
+                       degree_type='bachelor',
+                       record={
+                           "$ref": "http://180"
+                       },
+                       curated=True)
+
+    expected = [{
+        "name": 'Torres, Riccardo',
+        "ids": [{
+            "schema": "DESY",
+            "value": "DESY-55924820881"
+        },
+            {
+            "schema": "SCOPUS",
+            "value": "7039712595"
+        },
+            {
+            "schema": "SCOPUS",
+            "value": "8752067273"
+        }],
+        "degree_type": 'bachelor',
+        "record": {
+            "$ref": "http://180"
+        },
+        "curated_relation": True
+    }]
+    result = author.obj['advisors']
+
+    assert validate(result, subschema) is None
+    assert expected == result
+
+
+def test_add_advisor_normalizes_name():
+    schema = load_schema('authors')
+    subschema = schema['properties']['advisors']
+
+    author = AuthorBuilder()
+    author.add_advisor('Riccardo Torres Jr')
+
+    expected = [{
+        "name": 'Torres, Riccardo, Jr.',
+        "curated_relation": False
+    }]
+    result = author.obj['advisors']
 
     assert validate(result, subschema) is None
     assert expected == result
