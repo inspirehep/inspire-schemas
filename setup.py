06ae6223811909c17603b516d917650d2367dc42
Harris Tzovanakis <me@drjova.com>
2024-06-10 14:16:55 +0200

    setup: use inspire-idutils

--- setup.py
+++ setup.py
@@ -176,7 +176,7 @@ tests_require = [
     'pytest~=3.0,>=3.6.0',
     'pytest-pep8',
     'mock',
-    'idutils==1.2.1',
+    'inspire-idutils==1.2.2',
     'isbnid',
 ]
 
@@ -213,7 +213,7 @@ def do_setup():
             'bleach~=3.0,>=3.2.1',
             'Unidecode~=1.0,>=1.0.22',
             'jsonschema~=2.0,>=2.6.0',
-            'idutils==1.2.1',
+            'inspire-idutils==1.2.2',
             'isbnid',
             'inspire-utils~=3.0,>=3.0.0',
             'isodate',
